a68ed78a958fb813ed8f305cf67235dcfb2c0a56
CarlosF11 <cferna11@hotmail.es>
2019-05-02 10:55:48 +0200

    1
    1

---
+++ Assets/Scripts/BallLaunch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLaunch : MonoBehaviour
+{
+    public float force = 100;
+    public float maxForce = 200;
+    public float minForce = 25;
+
+    private Vector3 direction;
+
+    public Rigidbody rb;
+    private float forceChangeAmount = 25;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (Vector3)(mouse - transform.position);
+
+        if (Input.GetMouseButtonDown(0) && rb.velocity == Vector3.zero)
+        {
+            rb.AddForce(direction);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && force < maxForce)
+        {
+            force += forceChangeAmount;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && force > minForce)
+        {
+            force -= forceChangeAmount;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text welcomeText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        welcomeText.text = "WELCOME TO MINI GOLF SIMULATOR! GET 21 CUBES TO WIN THE GAME! USE THE ARROWS OR A-W-S-D TO CONTROL THE BALL";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString() + "/21";
+        if (count > 0)
+        {
+            welcomeText.text = "";
+        }
+        if (count >= 21)
+        {
+            winText.text = "CONGRATULATIONS! YOU'VE COLLECTED ALL THE CUBES";
+        }
+    }
+}
